b0daee92d2694552e126aca205e5dc2dbefd6a5a
qlq3763 <qlq3763@gmail.com>
2013-08-09 15:51:06 +0800

    clean up ex2.3

--- answer/c-2/ex2.3.scm
+++ answer/c-2/ex2.3.scm
@@ -3,47 +3,50 @@
 ;;     |
 ;;     |
 ;; p-e .
-(define (make-rectangle p-s p-m p-e)
-  (cons p-s (cons p-m p-e)))
+;; (define (make-rectangle p-s p-m p-e)
+;;   (cons p-s (cons p-m p-e)))
 
-(define (p-s r)
-  (car r))
+;; (define (p-s r)
+;;   (car r))
 
-(define (p-m r)
-  (cadr r))
+;; (define (p-m r)
+;;   (cadr r))
 
-(define (p-e r)
-  (cddr r))
+;; (define (p-e r)
+;;   (cddr r))
 
+;; The interface(abstraction barrier) is consisted of procedures 
+;; height-rectangle and width-rectangle. As far as this exercise is
+;; concerned, this is true.
 (define (height-rectangle r)
-  (let ((ps (p-s r))
-		(pm (p-m r)))
-    (sqrt (+ (square (- (x-point ps) (x-point pm)))
-			 (square (- (y-point ps) (y-point pm)))))))
+  (let ((pe (p-e r))
+	(pm (p-m r)))
+    (sqrt (+ (square (- (x-point pe) (x-point pm)))
+	     (square (- (y-point pe) (y-point pm)))))))
 
 (define (width-rectangle r)
   (let ((pm (p-m r))
-	(pe (p-e r)))
-    (sqrt (+ (square (- (x-point pm) (x-point pe)))
-	     (square (- (y-point pm) (y-point pe)))))))
+	(ps (p-s r)))
+    (sqrt (+ (square (- (x-point pm) (x-point ps)))
+	     (square (- (y-point pm) (y-point ps)))))))
 
 ;;    ------------. c1
 ;;    |
 ;;    |
 ;;    |
 ;; c2 .
-;; (define (make-rectangle c1 c2)
-;;   (cons c1 c2))
+(define (make-rectangle c1 c2)
+  (cons c1 c2))
 
-;; (define (p-s r)
-;;   (car r))
+(define (p-s r)
+  (car r))
 
-;; (define (p-e r)
-;;   (cdr r))
+(define (p-e r)
+  (cdr r))
 
-;; (define (p-m r)
-;;   (make-point (x-point (p-e r))
-;; 	      (y-point (p-s r))))
+(define (p-m r)
+  (make-point (x-point (p-e r))
+	      (y-point (p-s r))))
 
 
 (define (perimeter-rectangle r)
@@ -59,14 +62,14 @@
 ;; To test both versions, you have to comment out one version
 ;; at a time, then another version.
 
-;; (define r (make-rectangle ps pe))
-;; (newline)
-;; (display (perimeter-rectangle r))
-;; (newline)
-;; (display (area-rectangle r))
+(define r (make-rectangle ps pe))
+(newline)
+(display (perimeter-rectangle r))
+(newline)
+(display (area-rectangle r))
 
-(define r2 (make-rectangle ps pm pe))
-(newline)
-(display (perimeter-rectangle r2))
-(newline)
-(display (area-rectangle r2))
+;; (define r2 (make-rectangle ps pm pe))
+;; (newline)
+;; (display (perimeter-rectangle r2))
+;; (newline)
+;; (display (area-rectangle r2))
